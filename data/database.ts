2ee1d82983bf28d7668d6d1150c5378226e38d2c
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-07-19 16:38:21 +1000

    database setup

--- data/database.ts
+++ data/database.ts
@@ -1,10 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  database: 'marketplace_app',
-  user: 'root',
-  password: 'Database1991@',
+  host: 'us-cdbr-east-06.cleardb.net',
+  database: 'heroku_d1337da861f75bf',
+  user: 'be52b63b3bfc58',
+  password: '3e1f8a99',
 });
 
 export default pool;
+
