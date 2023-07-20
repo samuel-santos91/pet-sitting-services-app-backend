8976da5b03f1fcb9aa4ed76235bba2b87da3c244
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-07-20 20:57:09 +1000

    trying fix cors

--- index.ts
+++ index.ts
@@ -8,10 +8,22 @@ import sitterRequestPage from "./routes/sitter_request_page";
 
 const app: Express = express();
 
+const allowedOrigins = [
+  "https://64b8f1b9900f0661a42c3cb3--clinquant-frangollo-bba5e1.netlify.app",
+  "http://127.0.0.1:5173",
+  // Add more allowed origins if needed
+];
+
 app.use(
   cors({
-    origin:
-      "https://64b8f1b9900f0661a42c3cb3--clinquant-frangollo-bba5e1.netlify.app",
+    origin: function (origin, callback) {
+      // Check if the request's origin is in the allowed list
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["POST", "GET"],
     credentials: true,
   })
