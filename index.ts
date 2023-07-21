0a452ed40561dff8a327bd72c70bb51c686f25de
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-07-21 16:53:56 +1000

    fix cors

--- index.ts
+++ index.ts
@@ -8,20 +8,8 @@ import sitterRequestPage from "./routes/sitter_request_page";
 
 const app: Express = express();
 
-app.use(
-  cors({
-    origin:
-      "https://64b8f1b9900f0661a42c3cb3--clinquant-frangollo-bba5e1.netlify.app",
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
-app.options("/log-in", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://64b8f1b9900f0661a42c3cb3--clinquant-frangollo-bba5e1.netlify.app");
-  res.header("Access-Control-Allow-Methods", "POST, GET");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.send();
-});
+app.use(cors());
+
 app.use(express.static("public"));
 app.use(express.json());
 
