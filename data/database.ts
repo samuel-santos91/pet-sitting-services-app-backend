bc96360f0f266b8c6e4514f296dd214df0349218
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-07-25 23:49:03 +1000

    database host fix

--- data/database.ts
+++ data/database.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'sql6.freesqldatabase.com	',
+  host: 'sql6.freesqldatabase.com',
   database: 'sql6635153',
   user: 'sql6635153',
   password: '18jgi9psXg',
