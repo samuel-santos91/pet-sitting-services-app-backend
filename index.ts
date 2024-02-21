f4a3f97e2b494227ccbe3efe2652c8f04641d807
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2024-02-21 19:11:28 +1100

    fix typo and cors config

--- index.ts
+++ index.ts
@@ -1,20 +1,25 @@
 import express, { Express } from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 
 import authRoutes from "./routes/authentication";
 import customerPage from "./routes/customer_page";
 import customerRequestPage from "./routes/customer_request_page";
 import sitterRequestPage from "./routes/sitter_request_page";
 
+interface CustomCorsOptions extends CorsOptions {
+  headers?: string[];
+}
+
 const app: Express = express();
 
-app.use(
-  cors({
-    origin: "https://pet-sitting-service-app.netlify.app",
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+const corsOptions: CustomCorsOptions = {
+  origin: "https://pet-sitting-service-app.netlify.app",
+  methods: ["POST", "GET"],
+  credentials: true,
+  headers: ["Content-Type", "Authorization"], // Add your custom headers here
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -24,7 +29,7 @@ app.use(customerPage);
 app.use(customerRequestPage);
 app.use(sitterRequestPage);
 
-const port = process.env.MQSQL_ADDON_PORT || 3000;
+const port = process.env.MYSQL_ADDON_PORT || 3000;
 
 app.listen(port, () => {
   console.log("server is running");
